76d7013604da6773c02c5b48e3c9305a1abcb840
James Robert Rooke <thepoltergeist23@gmail.com>
2018-07-07 14:29:22 +0800

    Creating get/post route for comments endpoint and creating fetching live data from server in CommentBox

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -7,18 +7,46 @@ import './CommentBox.css';
 class CommentBox extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      error: null,
+      author: '',
+      text: ''
+    };
+    this.pollInterval = null;
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillMount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then( (response) => {
+        if (!response.success) this.setState({ error: response.error });
+        else this.setState({ data: response.data });
+      });
   }
   render() {
     return (
       <div className="container">
         <div className="comments">
           <h2>Comments:</h2>
-          <CommentList data={DATA} />
+          <CommentList data={this.state.data} />
         </div>
         <div className="form">
-          <CommentForm />
+          <CommentForm author={this.state.author} text={this.state.text} />
         </div>
+        {this.state.error && <p>{this.state.error}</p>}
       </div>
     );
   }
